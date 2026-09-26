b8f4ca7d09aed8f61d5f0b468c6ba30aba06a980
SAMURAI <66764345+xesdoog@users.noreply.github.com>
2026-09-26 02:03:11 +0100

    docs(Lua API): add /shared directory

---
+++ docs/Lua API/shared/icons.lua
@@ -0,0 +1,84 @@
+--[[
+Copyright (C) 2025 SAMURAI (xesdoog) & Contributors
+This file is part of YLP.
+
+YLP is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+YLP is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with YLP.  If not, see <https://www.gnu.org/licenses/>.
+]]
+
+
+return {
+	ICON_MS_ARROW_MENU_CLOSE                         = "\xEF\x8F\x93", -- U+F3D3
+	ICON_MS_ARROW_MENU_OPEN                          = "\xEF\x8F\x92", -- U+F3D2
+	ICON_MS_BACKGROUND_REPLACE                       = "\xEF\x88\x8A", -- U+F20A
+	ICON_MS_BACKSPACE                                = "\xEE\x85\x8A", -- U+E14A
+	ICON_MS_BLOCK                                    = "\xEF\x82\x8C", -- U+F08C
+	ICON_MS_BOOK                                     = "\xEE\xA1\xAE", -- U+E86E
+	ICON_MS_BRIGHTNESS_1                             = "\xEE\x8F\xBA", -- U+E3FA
+	ICON_MS_BROWSE                                   = "\xEE\xAC\x93", -- U+EB13
+	ICON_MS_CHECK_CIRCLE                             = "\xEF\x82\xBE", -- U+F0BE
+	ICON_MS_CHECK_CIRCLE_OUTLINE                     = "\xEF\x82\xBE", -- U+F0BE
+	ICON_MS_CLEAR                                    = "\xEE\x97\x8D", -- U+E5CD
+	ICON_MS_CLEAR_ALL                                = "\xEE\x82\xB8", -- U+E0B8
+	ICON_MS_CLOUD_DOWNLOAD                           = "\xEE\x8B\x80", -- U+E2C0
+	ICON_MS_CODE                                     = "\xEE\xA1\xAF", -- U+E86F
+	ICON_MS_CONTENT_COPY                             = "\xEE\x85\x8D", -- U+E14D
+	ICON_MS_DELETE                                   = "\xEE\xA4\xAE", -- U+E92E
+	ICON_MS_DISPLAY_SETTINGS                         = "\xEE\xAE\x97", -- U+EB97
+	ICON_MS_DOWNLOAD                                 = "\xEF\x82\x90", -- U+F090
+	ICON_MS_ERROR                                    = "\xEF\xA2\xB6", -- U+F8B6
+	ICON_MS_EXCLAMATION                              = "\xEF\x88\xAF", -- U+F22F
+	ICON_MS_EXTENSION                                = "\xEE\xA1\xBB", -- U+E87B
+	ICON_MS_FILE_COPY                                = "\xEE\x85\xB3", -- U+E173
+	ICON_MS_FILTER_LIST                              = "\xEE\x85\x92", -- U+E152
+	ICON_MS_FOLDER                                   = "\xEE\x8B\x87", -- U+E2C7
+	ICON_MS_HELP                                     = "\xEE\xA3\xBD", -- U+E8FD
+	ICON_MS_HOME_APP_LOGO                            = "\xEE\x8A\x95", -- U+E295
+	ICON_MS_HOURGLASS_BOTTOM                         = "\xEE\xA9\x9C", -- U+EA5C
+	ICON_MS_HOURGLASS_EMPTY                          = "\xEE\xA2\x8B", -- U+E88B
+	ICON_MS_HOURGLASS_TOP                            = "\xEE\xA9\x9B", -- U+EA5B
+	ICON_MS_INFO                                     = "\xEE\xA2\x8E", -- U+E88E
+	ICON_MS_LIST                                     = "\xEE\xA2\x96", -- U+E896
+	ICON_MS_MEMORY                                   = "\xEE\x8C\xA2", -- U+E322
+	ICON_MS_MESSAGE                                  = "\xEE\x83\x89", -- U+E0C9
+	ICON_MS_MORE_VERT                                = "\xEE\x97\x94", -- U+E5D4
+	ICON_MS_NOTIFICATIONS                            = "\xEE\x9F\xB5", -- U+E7F5
+	ICON_MS_NOTIFICATIONS_ACTIVE                     = "\xEE\x9F\xB7", -- U+E7F7
+	ICON_MS_NOTIFICATIONS_OFF                        = "\xEE\x9F\xB6", -- U+E7F6
+	ICON_MS_NOTIFICATIONS_PAUSED                     = "\xEE\x9F\xB8", -- U+E7F8
+	ICON_MS_NOTIFICATIONS_UNREAD                     = "\xEF\x93\xBE", -- U+F4FE
+	ICON_MS_OPACITY                                  = "\xEE\xA4\x9C", -- U+E91C
+	ICON_MS_OPEN_IN_NEW                              = "\xEE\xA2\x9E", -- U+E89E
+	ICON_MS_PALETTE                                  = "\xEE\x90\x8A", -- U+E40A
+	ICON_MS_PLAY_ARROW                               = "\xEE\x80\xB7", -- U+E037
+	ICON_MS_PRIORITY_HIGH                            = "\xEE\x99\x85", -- U+E645
+	ICON_MS_RADIO_BUTTON_CHECKED                     = "\xEE\xA0\xB7", -- U+E837
+	ICON_MS_RADIO_BUTTON_UNCHECKED                   = "\xEE\xA0\xB6", -- U+E836
+	ICON_MS_REFRESH                                  = "\xEE\x97\x95", -- U+E5D5
+	ICON_MS_RUNNING_WITH_ERRORS                      = "\xEE\x94\x9D", -- U+E51D
+	ICON_MS_SDK                                      = "\xEE\x9C\xA0", -- U+E720
+	ICON_MS_SEARCH                                   = "\xEE\xBD\xBA", -- U+EF7A
+	ICON_MS_SETTINGS                                 = "\xEE\xA2\xB8", -- U+E8B8
+	ICON_MS_STAR                                     = "\xEF\x82\x9A", -- U+F09A
+	ICON_MS_SYNC                                     = "\xEE\x98\xA7", -- U+E627
+	ICON_MS_SYRINGE                                  = "\xEE\x84\xB3", -- U+E133
+	ICON_MS_TERMINAL                                 = "\xEE\xAE\x8E", -- U+EB8E
+	ICON_MS_TOGGLE_OFF                               = "\xEE\xA7\xB5", -- U+E9F5
+	ICON_MS_TOGGLE_ON                                = "\xEE\xA7\xB6", -- U+E9F6
+	ICON_MS_TUNE                                     = "\xEE\x90\xA9", -- U+E429
+	ICON_MS_UPDATE                                   = "\xEE\xA4\xA3", -- U+E923
+	ICON_MS_UPLOAD_FILE                              = "\xEE\xA7\xBC", -- U+E9FC
+	ICON_MS_VOLUME_MUTE                              = "\xEE\x81\x8E", -- U+E04E
+	ICON_MS_VOLUME_UP                                = "\xEE\x81\x90", -- U+E050
+	ICON_MS_WARNING                                  = "\xEF\x82\x83", -- U+F083
+}
